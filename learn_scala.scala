43cc9a338a29abc4039020b9c019bb870258a93a
Alex Bennett <alexandergraybennett@gmail.com>
2016-05-27 16:29:54 -0600

    init

---
+++ learn_scala.scala
@@ -0,0 +1,15 @@
+def add(x:Int, y:Int):Int = {
+  return x + y
+}
+println(add(42,13))
+
+def subtract(a:Int, b:Int):Int = {
+  return a - b
+}
+
+println(subtract(32, 11))
+
+def multiply(c:Int, d:Int):Int = c * d
+
+
+println(multiply(2,8))
