1c6711fa5ec158f032e57b8ac19ec03831ec53b4
Alex Bennett <alexandergraybennett@gmail.com>
2016-05-27 22:35:09 -0600

    tuples

--- practice_funcs.scala
+++ practice_funcs.scala
@@ -21,3 +21,19 @@ println(tuples._2)
 
 val tupledUp = ("yo", "lets", "inception", ("here", "we", "go"))
 println(tupledUp._4._2)
+
+var swappingVals = ("white", "tiger")
+def swappage(word: String, word2: String) = (word, word2)
+var swapped = swappage("honey", "badger")
+println(swapped)
+
+var (a,b,c) = (1,2,3)
+println((s"a $a, b $b c $c"))
+
+var sum = 0
+for ( i <- 2 until 4) {
+  sum += i
+}
+println(sum)
+
+println((0 until 5).sum)
